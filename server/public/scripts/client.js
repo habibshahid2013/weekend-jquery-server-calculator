aab9f385aad1f7dfd5604e3643d9aba8e6c0fb00
Hassan Shahid <habibshahid2013@gmail.com>
2021-07-31 10:59:26 -0500

    button clicks

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,14 @@
 $(document).ready(onReady);
-
+let clickedButton = "";
+let firstInput = 0 ;
+let secondInput = 0 ;
 
 function onReady(){
     console.log('So Ready 🥙');
     
+    $('#addBtn').on('click', reassignmen)
+    $('#subtractBtn').on('click', totalBtn)
+    $('#multiplyBtn').on('click', totalBtn)
+    $('#clearBtn').on('click', totalBtn)
+    $('#totalBtn').on('click', totalBtn)
 }
