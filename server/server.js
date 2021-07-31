1366bbf6b8127e925f111dc6dcb5edc355ff9830
Hassan Shahid <habibshahid2013@gmail.com>
2021-07-31 09:14:11 -0500

    server is running

---
+++ server/server.js
@@ -0,0 +1,10 @@
+console.log('Testing that my server express is working 💫');
+
+const express = require('express');
+
+const app = express();
+
+app.listen(5000,function(){
+    console.log('Calcualator app is up and running 📲');
+    
+});
