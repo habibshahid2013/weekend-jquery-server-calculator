d3fe6b77ca3c74f87d6eaab70b4597caccd57027
Hassan Shahid <habibshahid2013@gmail.com>
2021-07-31 09:42:47 -0500

    set up client js and Jquery

--- server/server.js
+++ server/server.js
@@ -4,8 +4,11 @@ const express = require('express');
 
 const app = express();
 
+//static assets
+app.use(express.static('./server/public'));
+
 const port = 5000;
 app.listen(port, function(){
-    console.log('Calcualator app is up and running 📲');
+    console.log('Calculator app is up and running 📲');
     
 });
